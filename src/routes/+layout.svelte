45ad6722279c2c49c870d1f2c0a1bda0dded687c
Lan <lanlebar6@gmail.com>
2026-05-28 19:43:23 +0200

    add Apollo snippet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,20 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<script>
+		function initApollo() {
+			var n = Math.random().toString(36).substring(7),
+				o = document.createElement('script');
+			o.src = 'https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=' + n;
+			o.async = true;
+			o.defer = true;
+			o.onload = function () {
+				window.trackingFunctions.onLoad({ appId: '6a116d32e7a78f000c23daf9' });
+			};
+			document.head.appendChild(o);
+		}
+		initApollo();
+	</script>
 </svelte:head>
 
 <div class="flex min-h-screen flex-col antialiased">
